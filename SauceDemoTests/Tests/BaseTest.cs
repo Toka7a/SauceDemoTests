938afc21edcf889589d31c453ab8e4ae101ef0d9
Teodor Dimov <toka7aa@gmail.com>
2025-03-15 12:00:05 +0200

    Add project files.

---
+++ SauceDemoTests/Tests/BaseTest.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SauceDemoTests.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SauceDemoTests.Tests
+{
+    public class BaseTest
+    {
+        protected IWebDriver driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddUserProfilePreference("profile.password_manager_enabled", false);
+            driver = new ChromeDriver(chromeOptions);
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+            driver.Dispose();
+        }
+
+
+        protected void Login(string username, string password)
+        {
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            var loginPage = new LoginPage(driver);
+            loginPage.InputUsername(username);
+            loginPage.InputPassword(password);
+            loginPage.ClickLoginButton();
+        }
+    }
+}
